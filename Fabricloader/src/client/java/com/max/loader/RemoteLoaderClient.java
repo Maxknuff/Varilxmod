9f0faff6ce142d82ea18155cf3b8886db6dd054a
Maxknuff <derknuffelbuffel@gmail.com>
2026-01-30 17:10:19 +0000

    Templates

---
+++ Fabricloader/src/client/java/com/max/loader/RemoteLoaderClient.java
@@ -0,0 +1,10 @@
+package com.max.loader;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RemoteLoaderClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
